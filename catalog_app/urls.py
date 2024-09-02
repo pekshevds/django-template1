f0bfbdbd6f94b8c769296cabe1a93d316ec275af
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-02 14:48:57 +0300

    refactoring

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -11,11 +11,11 @@ from catalog_app.views import (
 app_name = "catalog_app"
 
 urlpatterns = [
-    path("manufacturer/", ManufacturerView.as_view(), name="manufacturer"),
+    path("manufacturer/", ManufacturerView.as_view(), name="manufacturers"),
     path("manufacturer/<str:slug>/", ManufacturerView.as_view(), name="manufacturer"),
-    path("category/", CategoryView.as_view(), name="category"),
+    path("category/", CategoryView.as_view(), name="categories"),
     path("category/<str:slug>/", CategoryView.as_view(), name="category"),
-    path("good/", GoodView.as_view(), name="good"),
+    path("good/", GoodView.as_view(), name="goods"),
     path("good/<str:slug>/", GoodView.as_view(), name="good"),
     path("data/", DataView.as_view(), name="data"),
     path("update-catalog/", UpdateCatalogView.as_view(), name="update-catalog"),
